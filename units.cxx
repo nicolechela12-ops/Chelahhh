e012850df77d9b32f8ce07121b6a322114d903ce
nicolechela12-ops <nicolechela12@gmail.com>
2026-09-16 08:06:41 +0300

    Add files via upload

---
+++ units.cxx
@@ -0,0 +1,18 @@
+//program to display accademic units offered
+/*
+Author: Nicole
+Reg no: BCS_05_0567/2026
+Description: Program to display academic units offered
+Date: 15/9/2026
+*/
+#include <stdio.h> //scanf(),printf()
+int main() {
+    printf("academic units.\n");
+	printf("introduction.\n");
+	printf("data structures.\n");
+	printf("computer networks.\n");
+	printf("database system.\n");
+	printf("software engineering.\n");
+	return 0 ; //execution successful
+}
+	
